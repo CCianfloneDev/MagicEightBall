7cc0a9af7b3e080606bf22efabbde1018317c668
Cole Cianflone <colecianflone123@gmail.com>
2022-05-08 10:40:20 -0500

    Update Magic.js

--- js/Magic.js
+++ js/Magic.js
@@ -1,17 +1,15 @@
 let spinButton = document.getElementById("magicButton");
 
-
-
 /*
     populateImages function
     Using the dog API dataset populate existing image elements
-*//*
+*/
 function populatefactFetch() {
   let randomFactText = document.getElementById("randomFactText");
 
   fetch("http://numbersapi.com/random/trivia")
   .then(data => { 
     randomFactText.textContent = `${data.type}`});
-}*/
+}
 
 spinButton.addEventListener("click", populatefactFetch);
