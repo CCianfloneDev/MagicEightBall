eaeade9ef8ef18a9ab46a5eb8e1059bd03eaf707
Cole Cianflone <colecianflone123@gmail.com>
2022-05-15 13:12:10 -0500

    Update Magic.js

--- js/Magic.js
+++ js/Magic.js
@@ -1,14 +1,21 @@
 /*
     populatefactFetch function
-    Using the Numbers API dataset populate existing image elements
+    Using the facts api from: https://api-ninjas.com/api/facts
 */
 function populatefactFetch() {
-  let randomFactText = document.getElementById("randomFactText");
-
-  fetch("http://numbersapi.com/random/trivia")
-  .then(response => response.text())
-  .then(data => { 
-    randomFactText.textContent = `${data}`});
+  $.ajax({
+      method: 'GET',
+      url: 'https://api.api-ninjas.com/v1/facts?limit=1',
+      // api key associated with my account on https://api-ninjas.com/profile
+      headers: { 'X-Api-Key': 'ZLAxS/Jh/3J3HCto2TULLQ==aWjW5z3sRgxdk0Ih'},
+      contentType: 'application/json',
+      success: function(result) {
+          console.log(result);
+      },
+      error: function ajaxError(jqXHR) {
+          console.error('Error: ', jqXHR.responseText);
+      }
+  });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
