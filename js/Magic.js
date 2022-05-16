7aee1779f05ab49738f58c5c32be1a8a32cde448
Cole Cianflone <colecianflone123@gmail.com>
2022-05-15 20:55:23 -0500

    Update Magic.js

--- js/Magic.js
+++ js/Magic.js
@@ -1,21 +1,14 @@
 /*
     populatefactFetch function
-    Using the facts api from: https://api-ninjas.com/api/facts
+    Using: https://fungenerators.com/api/facts/#apidoc
 */
 function populatefactFetch() {
-  $.ajax({
-      method: 'GET',
-      url: 'https://api.api-ninjas.com/v1/facts?limit=1',
-      // api key associated with my account on https://api-ninjas.com/profile
-      headers: { 'X-Api-Key': 'ZLAxS/Jh/3J3HCto2TULLQ==aWjW5z3sRgxdk0Ih'},
-      contentType: 'application/json',
-      success: function(result) {
-          console.log(result);
-      },
-      error: function ajaxError(jqXHR) {
-          console.error('Error: ', jqXHR.responseText);
-      }
-  });
+  let randomFactText = document.getElementById("randomFactText");
+
+  fetch("https://api.fungenerators.com/fact/random")
+  .then(response => response.text())
+  .then(data => { 
+    randomFactText.textContent = `${data}`});
 }
 
 document.addEventListener("DOMContentLoaded", () => {
